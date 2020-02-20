aeecc530d1118315f70e02e2e0ee1cf848cf7dfd
Christopher Leja <christopher.leja@gmail.com>
2020-02-20 16:26:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
   end
   
   def apply_discount
-    if @discount != 0
+    total_reduce(total, discount)
       puts "After the discount, the total comes to #{@total}"
     else
       @total
